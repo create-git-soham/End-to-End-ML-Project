2f712b5299fa430a9b6eacad2a7256cc8c8b4651
create-git-soham <sohammarawade07@gmail.com>
2026-03-08 17:17:54 +0530

    Hyperparameter Training

--- src/utlis.py
+++ src/utlis.py
@@ -1,11 +1,12 @@
 import os 
 import sys
+import dill
 
 import numpy as np
 import pandas as pd
-import dill
 from sklearn.metrics import r2_score
 
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -20,14 +21,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys) 
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            # model = list(models.values())[i]
+            para = param.get(model_name, {})
 
-            model.fit(X_train, y_train) # Train Models
+            gs = GridSearchCV(model,para,cv = 3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train) # Train Models
 
             y_train_pred = model.predict(X_train)
 
@@ -37,8 +45,18 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
             
         return report 
     except Exception as e:
         raise CustomException(e, sys) 
+    
+def load_object(file_path):
+    """
+    Helper function to load serialized objects (preprocessor/model)
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
